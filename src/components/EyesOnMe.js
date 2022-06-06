2bc6fc304fa386c33d81dbe7768f7c2d64e776be
Antony Caseres <accasere@outlook.com>
2022-06-06 00:53:36 -0500

    Completed Assigment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+
+    function handleClick(event){
+        if(event._reactName === "onBlur"){
+            console.log("Hey! Eyes on me!")
+        } else {
+            console.log("Good!")
+        }
+    }
+  
+   return (
+    <button onFocus={handleClick} onBlur={handleClick}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad(){
+    function onHandleChange(){
+        console.log("Entering password...")
+    }
+    return(
+        <div>
+            <input
+                 type="password"
+                 name="password"
+                 onChange={onHandleChange}
+                 placeholder="Enter search term..."
+            />
+        </div>
+    )
+}
+
+export default Keypad;
